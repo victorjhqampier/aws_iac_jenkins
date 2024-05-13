c77fa4a6cf363beaaf0ab51b4dafec1aec766167
frontend-at-683418597746 <vcaxi@cajalosandes.pe>
2024-05-13 15:19:29 -0500

    Se cra s3

--- main.tf
+++ main.tf
@@ -4,10 +4,10 @@ provider "aws" {
   secret_key = var.iac_aws_secret_key
 }
 
-# module "s3" {
-#   source = "git::https://github.com/victorjhqampier/aws_s3_module.git//minimal"
-#   # Send Variables
-#   aws_project_id = var.aws_project_id
-#   aws_project_env = var.aws_project_env
-#   aws_project_account = var.aws_project_account
-# }
+module "s3" {
+  source = "git::https://github.com/victorjhqampier/aws_s3_module.git//minimal"
+  # Send Variables
+  aws_project_id = var.aws_project_id
+  aws_project_env = var.aws_project_env
+  aws_project_account = var.aws_project_account
+}
